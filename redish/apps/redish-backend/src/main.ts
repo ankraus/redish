0f981328d37093ada1b01958065728cbe2b1caed
Tim Kühnlein <kuehnleintim@gmail.com>
2023-10-13 22:48:29 +0100

    b add swagger

--- redish/apps/redish-backend/src/main.ts
+++ redish/apps/redish-backend/src/main.ts
@@ -7,16 +7,30 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  // cors
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET',
+  });
+
+  // swagger api documentation
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('redish')
+    .setDescription('redish API')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
