d92b71a9c648b6d8f69bfeb45b9ab19d67c10c71
Tim Kühnlein <kuehnleintim@gmail.com>
2023-10-18 14:27:38 +0100

    fb add git pre-commit hook, add tags for all libraries and documentation for that in readme

--- redish/apps/redish-backend/src/app/app.module.ts
+++ redish/apps/redish-backend/src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { RestApiModule } from '@redish-backend/rest-api';
-import { ConfigurationModule } from 'libs/redish-backend/shared/src/lib/config/config.module';
 
 @Module({
-  imports: [
-    RestApiModule,
-    ConfigurationModule
-  ],
+  imports: [RestApiModule],
   controllers: [AppController],
 })
 export class AppModule {}
