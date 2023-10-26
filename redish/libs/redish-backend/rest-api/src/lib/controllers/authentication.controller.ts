a52f5a94b4037b664ac11d6db0b0e8f871c3fc24
Andreas Kraus <andreas.kraus.97@gmail.com>
2023-10-26 15:45:26 +0100

    b - make register route return correct errors and add openapi doc

--- redish/libs/redish-backend/rest-api/src/lib/controllers/authentication.controller.ts
+++ redish/libs/redish-backend/rest-api/src/lib/controllers/authentication.controller.ts
@@ -2,18 +2,30 @@ import {
   Body,
   Controller,
   Get,
+  HttpStatus,
   Injectable,
   Post,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { AuthenticationFacade } from '@redish-backend/usecases';
 import { Result } from '@redish-backend/domain';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthenticateUserDto } from '../dtos/authenticate-user.dto';
 import { TokenDto } from '../dtos/token.dto';
 import { UuidDto } from '../dtos/uuid.dto';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '../guards/auth.guard';
+import { Response } from 'express';
+import { RedishErrorDto } from '../dtos/redish-error.dto';
+import { RedishError } from '@redish-shared/domain';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -29,14 +41,33 @@ export class AuthenticationController {
     return this.authenticationFacade.authenticateUser(authenticateUserDto);
   }
 
-  @ApiCreatedResponse({ type: Result<UuidDto> })
+  @ApiCreatedResponse({ type: UuidDto })
+  @ApiBadRequestResponse({ type: RedishErrorDto })
+  @ApiInternalServerErrorResponse({ type: RedishErrorDto })
   @Post('register')
   async createUser(
+    @Res() response: Response,
     @Body() createUserDto: CreateUserDto
-  ): Promise<Result<UuidDto>> {
-    return this.authenticationFacade.createUser(createUserDto);
+  ): Promise<UuidDto | RedishErrorDto> {
+    const createUserResult = await this.authenticationFacade.createUser(
+      createUserDto
+    );
+    if (createUserResult.error) {
+      if (
+        createUserResult.error.code ===
+        RedishError.Infrastructure.Codes.DATABASE_ERROR
+      ) {
+        response.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
+      } else {
+        response.status(HttpStatus.BAD_REQUEST).send();
+      }
+      return createUserResult.error;
+    } else {
+      return createUserResult.result!;
+    }
   }
 
+  @ApiBearerAuth()
   @Get('protected')
   @UseGuards(AuthGuard)
   async protectedRoute(): Promise<Result<string>> {
