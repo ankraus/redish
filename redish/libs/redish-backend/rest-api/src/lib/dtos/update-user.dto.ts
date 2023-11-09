5cd33ba095c9cea10c5c64afdbe4d502c392582f
Andreas Kraus <andreas.kraus.97@gmail.com>
2023-11-09 14:24:58 +0000

    b - add ApiProperty decorator to UpdateUserDto

--- redish/libs/redish-backend/rest-api/src/lib/dtos/update-user.dto.ts
+++ redish/libs/redish-backend/rest-api/src/lib/dtos/update-user.dto.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UpdateUserDto as IUpdateUserDto } from '@redish-shared/domain';
 
 export class UpdateUserDto implements IUpdateUserDto {
+  @ApiProperty()
   username?: string;
+  @ApiProperty()
   password?: string;
+  @ApiProperty()
   email?: string;
 }
