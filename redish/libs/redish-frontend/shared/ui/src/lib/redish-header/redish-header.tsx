ea464086adbaa6d09fc8249f6ac6356a24274732
Tim Kühnlein <kuehnleintim@gmail.com>
2023-12-06 01:14:20 +0000

    f - add burger menu

--- redish/libs/redish-frontend/shared/ui/src/lib/redish-header/redish-header.tsx
+++ redish/libs/redish-frontend/shared/ui/src/lib/redish-header/redish-header.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import RedishButton from '../redish-button/redish-button';
 import RedishLink from '../redish-link/redish-link';
 import styles from './redish-header.module.scss';
+import cn from 'classnames';
 
 export interface RedishHeaderProps {
   actions: Array<{ labelSmall?: string; label: string; onClick: () => void }>;
@@ -13,6 +15,8 @@ export function RedishHeader({
   navigation,
   handleLogoClicked,
 }: RedishHeaderProps) {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div className={styles.container}>
       <button
@@ -25,35 +29,46 @@ export function RedishHeader({
         <img src="assets/icons/redish-24-white.svg" alt="logo" />
         <h1>redish</h1>
       </button>
-      <div className={styles.actions}>
-        {actions.map((action) => (
-          <>
-            <RedishButton
-              className={styles.medium}
-              key={action.label}
-              onClick={action.onClick}
-            >
-              {action.label}
-            </RedishButton>
-            <RedishButton
-              className={styles.small}
-              key={`small-${action.labelSmall ?? action.label}`}
-              onClick={action.onClick}
-            >
-              {action.labelSmall ?? action.label}
-            </RedishButton>
-          </>
-        ))}
-      </div>
-      {navigation.length > 0 && (
-        <ul>
-          {navigation.map((nav) => (
-            <li key={nav.label}>
-              <RedishLink to={nav.to}>{nav.label}</RedishLink>
-            </li>
+      <button
+        className={styles.burgerMenu}
+        onClick={(event) => {
+          event.preventDefault();
+          setShowMenu(!showMenu);
+        }}
+      >
+        <img src="assets/icons/menu-icon.svg" alt="menu" />
+      </button>
+      <div className={cn(styles.content, showMenu && styles.showMenu)}>
+        <div className={styles.actions}>
+          {actions.map((action) => (
+            <>
+              <RedishButton
+                className={styles.medium}
+                key={action.label}
+                onClick={action.onClick}
+              >
+                {action.label}
+              </RedishButton>
+              <RedishButton
+                className={styles.small}
+                key={`small-${action.labelSmall ?? action.label}`}
+                onClick={action.onClick}
+              >
+                {action.labelSmall ?? action.label}
+              </RedishButton>
+            </>
           ))}
-        </ul>
-      )}
+        </div>
+        {navigation.length > 0 && (
+          <ul>
+            {navigation.map((nav) => (
+              <li key={nav.label}>
+                <RedishLink to={nav.to}>{nav.label}</RedishLink>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
