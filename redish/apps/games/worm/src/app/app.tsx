1ce270ccc89e636d6e11475953c95c3f0b7f0903
Tim Kühnlein <kuehnleintim@gmail.com>
2023-12-07 12:40:31 +0000

    f - worm: add word validation

--- redish/apps/games/worm/src/app/app.tsx
+++ redish/apps/games/worm/src/app/app.tsx
@@ -1,6 +1,7 @@
 import styles from './app.module.scss';
 import { WormAppProps as AppProps } from '@redish-frontend/shared-models';
 import { WormFeature } from '@redish-games/worm-feature';
+import { ToastContainer } from 'react-toastify';
 
 export function App(appProps: Readonly<AppProps>) {
   const username = appProps.username ?? 'test';
@@ -8,6 +9,18 @@ export function App(appProps: Readonly<AppProps>) {
   return (
     <div className={styles.container}>
       <WormFeature username={username} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
--- redish/libs/games/worm/data-access/src/lib/worm.facade.ts
+++ redish/libs/games/worm/data-access/src/lib/worm.facade.ts
@@ -1,5 +1,8 @@
 import { WormGameState } from '@redish-games/worm-models';
 import { useImmer } from 'use-immer';
+import { gameUtilityApiService } from './connector/game-utility-api.service';
+import { Result } from '@redish-shared/domain';
+import { toast } from 'react-toastify';
 
 /**
  * facade hook as described in https://thomasburlesonia.medium.com/https-medium-com-thomasburlesonia-react-hooks-rxjs-facades-4e116330bbe1
@@ -44,10 +47,23 @@ export function useWormFacade(): {
     });
   };
 
-  const handleWormGameSubmitWord = () => {
-    updateWormGame((draft) => {
-      draft.currentWordIndex = draft.currentIndex;
-    });
+  const handleWormGameSubmitWord = async () => {
+    const word = wormGame.word.slice(
+      Math.max(0, wormGame.currentWordIndex - 1),
+      wormGame.currentIndex
+    );
+    const isWordResult = await verify(word.join(''));
+    if (!isWordResult.success) return;
+
+
+    console.info('verify', word, isWordResult.result);
+    if (isWordResult.result) {
+      updateWormGame((draft) => {
+        draft.currentWordIndex = draft.currentIndex;
+      });
+    } else {
+      toast.error('Not a word');
+    }
   };
 
   return {
@@ -59,4 +75,9 @@ export function useWormFacade(): {
       handleSubmitWord: handleWormGameSubmitWord,
     },
   };
+}
+
+async function verify(word: string): Promise<Result<boolean>> {
+  if (word.length === 0) return Result.success(false);
+  return await gameUtilityApiService.verify(word);
 }
